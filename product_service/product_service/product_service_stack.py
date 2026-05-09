7f34093e854c2cea35538816560d5f3c5d9a65cb
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-05-09 21:22:04 +0400

    Integrate DynamoDB with lambda functions

--- product_service/product_service/product_service_stack.py
+++ product_service/product_service/product_service_stack.py
@@ -3,6 +3,8 @@ from aws_cdk import (
     CfnOutput,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_dynamodb as dynamodb,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,12 +12,32 @@ class ProductServiceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        products_table = dynamodb.Table(self, 'ProductsTable',
+            table_name='products',
+            partition_key=dynamodb.Attribute(name='id', type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+    
+        stocks_table = dynamodb.Table(self, 'StocksTable',
+            table_name='stocks',
+            partition_key=dynamodb.Attribute(name='product_id', type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        common_env = {
+            'PRODUCTS_TABLE': products_table.table_name,
+            'STOCKS_TABLE': stocks_table.table_name,
+        }
+
         get_products_list = _lambda.Function(
             self, "GetProductsListFn",
             function_name="getProductsList",
             runtime=_lambda.Runtime.PYTHON_3_12,
             code=_lambda.Code.from_asset("lambda"),
             handler="get_products_list.handler",
+            environment=common_env,
         )
 
         get_products_by_id = _lambda.Function(
@@ -24,6 +46,15 @@ class ProductServiceStack(Stack):
             runtime=_lambda.Runtime.PYTHON_3_12,
             code=_lambda.Code.from_asset("lambda"),
             handler="get_products_by_id.handler",
+            environment=common_env,
+        )
+
+        create_product = _lambda.Function(self, 'CreateProduct',
+            function_name='createProduct',
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler='create_product.handler',
+            code=_lambda.Code.from_asset('lambda'),
+            environment=common_env,
         )
 
         api = apigw.RestApi(
@@ -34,11 +65,16 @@ class ProductServiceStack(Stack):
                 allow_methods=apigw.Cors.ALL_METHODS,
             ),
         )
+        
+        for fn in [get_products_list, get_products_by_id, create_product]:
+            products_table.grant_read_write_data(fn)
+            stocks_table.grant_read_write_data(fn)
 
         products = api.root.add_resource("products")
         products.add_method("GET", apigw.LambdaIntegration(get_products_list))
 
         product_by_id = products.add_resource("{productId}")
         product_by_id.add_method("GET", apigw.LambdaIntegration(get_products_by_id))
+        products.add_method("POST", apigw.LambdaIntegration(create_product))
 
         CfnOutput(self, "ApiUrl", value=api.url)
